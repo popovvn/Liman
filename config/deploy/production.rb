aef325c4bba7937980b5cce12401d4a2503aba42
Vladimir Popov <vpopov.perm@yandex.ru>
2020-08-18 23:32:33 +0500

    initial deploy

---
+++ config/deploy/production.rb
@@ -0,0 +1,7 @@
+
+server '89.223.24.167', user: 'root', roles: %w{app db web}
+
+set :application, "liman"
+set :deploy_to, -> { "/srv/#{fetch(:application)}" }
+set :rails_env, "production"
+set :rvm_custom_path, '/usr/local/rvm'
