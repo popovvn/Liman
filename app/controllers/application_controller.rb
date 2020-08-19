06987242166be19a42e62cddedf08b5f10646270
Vladimir Popov <vpopov.perm@yandex.ru>
2020-08-19 14:17:35 +0500

    without autentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: 'Liman', password: 'LiQuad1Man25!'
+  #http_basic_authenticate_with name: 'Liman', password: 'LiQuad1Man25!'
 end
