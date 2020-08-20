5c71d42a84d9cfead4351912f6779b8a8fac237d
Vladimir Popov <vpopov.perm@yandex.ru>
2020-08-20 15:28:44 +0500

    json render

--- app/controllers/license_controller.rb
+++ app/controllers/license_controller.rb
@@ -2,9 +2,11 @@ class LicenseController < ApplicationController
   def get
     license = License.find_by lictype: params[:lictype], licaccount: params[:acc]
     if license
-      @ret = license.licdate
+      ret = license.licdate
     else
-      @ret = 'not exist'
+      ret = 'not exist'
     end
+    #render text: ret, content_type: 'text/plain'
+    render json: ret
   end
 end
