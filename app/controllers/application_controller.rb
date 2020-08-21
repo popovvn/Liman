0d0a676c7893ccd1ec95f5616ade817ef06f4e85
Nikita Popov <ningen.tos@gmail.com>
2020-08-21 11:38:00 +0500

    Active Adminbasic http authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
-  #http_basic_authenticate_with name: 'Liman', password: 'LiQuad1Man25!'
+  http_basic_authenticate_with :name => "frodo", :password => "thering", :if => :admin_controller?
+
+  def admin_controller?
+    self.class < ActiveAdmin::BaseController
+  end
 end
